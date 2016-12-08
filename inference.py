dfd2ce49a1fe966e549efa030f5123c7f7339582
Hao <dhsig552@163.com>
2016-12-08 21:54:44 +0000

    Update inference.py

--- inference.py
+++ inference.py
@@ -205,3 +205,70 @@ class siamese_tl2:
         losses = tf.add(pos, neg, name="losses")
         loss = tf.reduce_mean(losses, name="loss")
         return loss
+ 
+
+ 
+class siamese_tl3:
+
+    # Create model
+    def __init__(self):
+        batch_size = 20 # actually it is 10, 0~10 are x1,  11~20 are x2
+        self.x = tf.placeholder(tf.float32, [batch_size, 784])
+        self.h = self.network(self.x, reuse=False) 
+        self.o = self.network2(self.h[0:batch_size/2], self.h[batch_size/2:])
+                                        
+        # Create loss
+        self.y_ = tf.placeholder(tf.float32, [batch_size/2])
+        self.loss = self.loss_with_spring()
+
+    def network(self, x, reuse):
+        # Define the neural network structure
+        with tf.variable_scope("siamese", reuse=reuse):
+            tl.layers.set_name_reuse(reuse)
+            network = tl.layers.InputLayer(x, name='input_layer')
+            network = tl.layers.DenseLayer(network, n_units=1024, act = tf.nn.relu, name='relu1')
+            network = tl.layers.DenseLayer(network, n_units=1024, act = tf.nn.relu, name='relu2')
+            network = tl.layers.DenseLayer(network, n_units=1024, act = tf.identity, name='relu3')
+        return network.outputs
+
+    def network2(self, x1, x2):
+        net1 = tl.layers.InputLayer(x1, name='input_x1')   
+        net2 = tl.layers.InputLayer(x2, name='input_x2')
+        network = tl.layers.ConcatLayer([net1, net2], name='concat')  
+        network = tl.layers.DenseLayer(network, n_units=2, act = tf.identity, name='output_layer')
+        return network.outputs
+                                        
+    def loss_with_spring(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.sub(1.0, self.y_, name="1-yi")          # labels_ = !labels;
+        eucd2 = tf.pow(tf.sub(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        # yi*||CNN(p1i)-CNN(p2i)||^2 + (1-yi)*max(0, C-||CNN(p1i)-CNN(p2i)||^2)
+        pos = tf.mul(labels_t, eucd2, name="yi_x_eucd2")
+        # neg = tf.mul(labels_f, tf.sub(0.0,eucd2), name="yi_x_eucd2")
+        # neg = tf.mul(labels_f, tf.maximum(0.0, tf.sub(C,eucd2)), name="Nyi_x_C-eucd_xx_2")
+        neg = tf.mul(labels_f, tf.pow(tf.maximum(tf.sub(C, eucd), 0), 2), name="Nyi_x_C-eucd_xx_2")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
+
+    def loss_with_step(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.sub(1.0, self.y_, name="1-yi")          # labels_ = !labels;
+        eucd2 = tf.pow(tf.sub(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        pos = tf.mul(labels_t, eucd, name="y_x_eucd")
+        neg = tf.mul(labels_f, tf.maximum(0.0, tf.sub(C, eucd)), name="Ny_C-eucd")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
+    
+    def train(self, X_train, y_train):
+        sess.run(train_op, feed_dict={})
+        
